29d347222f979ee0c5042c8bf7c30347e979898a
root <root@deb>
2016-05-04 16:23:36 -0300

    1.0

--- notifai/notifai/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=notifai',
-    'username' => 'root',
-    'password' => 'ujqr87cet',
-    'charset' => 'utf8',
-];
